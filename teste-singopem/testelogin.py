9c0b85ba7ee936d2849390cfc3d9a091ccb4d42f
Mario Valente <84289809+Mariovalente@users.noreply.github.com>
2021-11-30 09:32:17 -0300

    finalizando o código de teste
    O código foi finalizado, com sucesso, porém as condicionais apresentam algum tipo de mal uso, devo verificar.

--- teste-singopem/testelogin.py
+++ teste-singopem/testelogin.py
@@ -1,6 +1,19 @@
 import selenium
-from datetime import time
+import time 
 
 #abrindo o navegador
-navegador  = webdriver.Chrome()
-navegador.get
+navegador = webdriver.Chrome()
+navegador.get('https://pt-br.facebook.com/')
+navegador.find_element_by_xpath('//*[@id="email"]').click()
+navegador.find_element_by_xpath('//*[@id="email"]').send_keys('mario123')
+navegador.find_element_by_xpath('//*[@id="pass"]').click()
+navegador.find_element_by_xpath('//*[@id="pass"]').send_keys('1234')
+time.sleep(2)
+navegador.find_element_by_name('login').click()
+time.sleep(10)
+
+if len(navegador.find_element_by_xpath('//*[@id="mount_0_0_F+"]/div/div[1]/div/div[2]/div[3]/div/div[1]/div[1]/ul')) == 1:
+    print('teste bem sucedido')
+else:
+    print('teste mal sucedido')
+
